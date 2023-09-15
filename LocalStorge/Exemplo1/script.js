0f364fd6b6a933520542e42ca0a709920d16164c
João Vitor Alves Ribeiro <joao.ribeiro@phb.com>
2023-09-15 16:35:21 -0300

    local e projeto

---
+++ LocalStorge/Exemplo1/script.js
@@ -0,0 +1,65 @@
+let CampoNome = document.getElementById("caixaNome")
+let btnSalvar = document.getElementById('salvar')
+
+let CampoExcluir = document.getElementById("caixaExcluir")
+let btnExcluir = document.getElementById("excluir")
+let novoAluno = 0
+CampoNome.value
+btnSalvar.addEventListener('click', salvarLocal)
+btnExcluir.addEventListener('click', excluir)
+
+function salvarLocal(){
+ 
+    console.log(verificarNome())
+    if(verificarNome() == false){
+    localStorage.setItem(novoAluno, CampoNome.value)
+    let nome = document.createElement("p")
+    nome.innerText = `bem vindo ${CampoNome.value} seu id é ${novoAluno}`
+    document.body.appendChild(nome)
+    novoAluno = novoAluno + 1
+} 
+}
+
+function verificarNome(){
+    let verificar //variavel que vai amazenar o nome a ser pesquisado
+    for(let i =0; i<= localStorage.length; i++){
+    verificar = localStorage.getItem(i) 
+    console.log(`o valor da primeira pesquisa foi: ${verificar} nome a ser pesquisado: ${CampoNome.value}`)
+
+    //aqui ele vai verificar se o nome do id i é igual o nome que foi digitado 
+    if(verificar == CampoNome.value){
+        console.log("esse nome ja exite") 
+        return true
+        break
+    }else{
+        console.log("nome nao existe")
+    }
+   console.log(i)
+   return false
+}
+    console.log("saiu do console")
+
+}
+
+function excluir(){
+
+    if(localStorage.getItem(CampoExcluir.value) != null){
+        localStorage.removeItem(CampoExcluir.value)  
+      let nome = document.createElement("h2")
+      nome.innerText = `o nome do id ${CampoExcluir.value} foi excluido com sucesso`
+      document.body.appendChild(nome)
+    }else{
+          let nome = document.createElement("h2")
+        nome.innerText = `o nome do id ${CampoExcluir.value} nao foi encontrado`
+        document.body.appendChild(nome)
+    }
+    
+      
+
+      
+    
+    
+}
+
+
+   
---
+++ LocalStorge/objeto/script.js
@@ -0,0 +1,42 @@
+let divMenu = document.getElementById("divMenu")
+let nav = document.getElementById("navBarra")
+
+
+let exibirMenu= () => {
+   nav.style.display = "flex"
+   nav.style.flexDirection = "column"
+   divMenu.style.height = "250px"
+   divMenu.style.width = "150px"
+   divMenu.style.backgroundColor = "rgb(211, 247, 5)"
+
+    }
+let esconderMenu = () => {
+    nav.style.display = "none"
+    divMenu.style.height = "5px"
+    divMenu.style.width = "5px"
+    divMenu.style.backgroundColor = "rgb(0, 0, 0)"
+    }
+
+
+/* let menu = `  
+<nav>
+<a href="excluir.html">excluir usuario</a>
+<a href="pesquisar.html">pesquisar usuario</a>
+<a href="listaUsuarios.html">lista todos usuarios</a>
+</nav>`
+
+let exibirMenu = () => {
+divMenu.innerHTML =  `  
+<nav>
+<a " href="excluir.html">excluir usuario</a>
+<a href="pesquisar.html">pesquisar usuario</a>
+<a href="listaUsuarios.html">lista todos usuarios</a>
+</nav>`
+}
+let esconderMenu = () => {
+    divMenu.innerHTML =  `  
+   `
+    }*/
+divMenu.addEventListener("mouseover", exibirMenu)
+
+divMenu.addEventListener("mouseout", esconderMenu)  
